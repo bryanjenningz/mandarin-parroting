6b55382ee4bf480ef5a1c3b9e1f26b40d95b2a61
Bryan Jennings <bryanjenningz@gmail.com>
2024-01-10 17:58:23 -0800

    Add view props to NewVideo.view

--- src/NewVideo.elm
+++ src/NewVideo.elm
@@ -2,6 +2,7 @@ module NewVideo exposing (NewVideo, view)
 
 import Html exposing (Html, article, button, div, h2, input, label, text, textarea)
 import Html.Attributes exposing (for, id)
+import Html.Events exposing (onClick, onInput)
 
 
 type alias NewVideo =
@@ -10,17 +11,35 @@ type alias NewVideo =
     }
 
 
-view : Html msg
-view =
+type alias ViewProps msg =
+    { setNewVideoId : String -> msg
+    , setNewVideoTranscript : String -> msg
+    , submitNewVideo : msg
+    }
+
+
+view : ViewProps msg -> Html msg
+view props =
     article []
         [ h2 [] [ text "Add a new video" ]
         , div []
-            [ label [ for "new-video-id-input" ] [ text "New video ID" ]
-            , input [ id "new-video-id-input" ] []
+            [ label [ for "new-video-id-input" ]
+                [ text "New video ID" ]
+            , input
+                [ id "new-video-id-input"
+                , onInput props.setNewVideoId
+                ]
+                []
             ]
         , div []
-            [ label [ for "new-video-transcript-textarea" ] [ text "New video transcript" ]
-            , textarea [ id "new-video-transcript-textarea" ] []
+            [ label [ for "new-video-transcript-textarea" ]
+                [ text "New video transcript" ]
+            , textarea
+                [ id "new-video-transcript-textarea"
+                , onInput props.setNewVideoTranscript
+                ]
+                []
             ]
-        , button [] [ text "Add video" ]
+        , button [ onClick props.submitNewVideo ]
+            [ text "Add video" ]
         ]
