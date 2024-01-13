925d8ec7da9721b666d9fbbfa1cd4fdca85f60b6
Bryan Jennings <bryanjenningz@gmail.com>
2024-01-13 04:20:30 -0800

    Add transcript to subtitles 3-line test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -96,6 +96,22 @@ transcriptToSubtitlesTests =
                             , { text = "Hi", time = 61 }
                             ]
                         )
+        , test "Returns 3 entries for a 3-line transcript" <|
+            \_ ->
+                NewVideo.transcriptToSubtitles
+                    """0:00
+各位同学大家好 我是李永乐老师
+0:02
+最近全国很多地方降温幅度非常大
+0:05
+很多小伙伴都在家里面"""
+                    |> Expect.equal
+                        (Ok
+                            [ { text = "各位同学大家好 我是李永乐老师", time = 0 }
+                            , { text = "最近全国很多地方降温幅度非常大", time = 2 }
+                            , { text = "很多小伙伴都在家里面", time = 5 }
+                            ]
+                        )
         ]
 
 
