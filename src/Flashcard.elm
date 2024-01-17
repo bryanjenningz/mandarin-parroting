fa65568a5cd86c6ed8603bf32a9cf3eb6654617e
Bryan Jennings <bryanjenningz@gmail.com>
2024-01-16 21:47:32 -0800

    Improve flashcard back styling

--- src/Flashcard.elm
+++ src/Flashcard.elm
@@ -94,12 +94,22 @@ view props =
                         text ""
                     ]
                 , if props.flashcardBackShown then
-                    div [ class "flex flex-col grow" ]
-                        [ div [] [ text flashcard.pinyin ]
-                        , div [] [ text (String.join "; " flashcard.definitions) ]
-                        , div []
-                            [ button [ onClick (props.failFlashcard flashcard) ] [ text "Fail" ]
-                            , button [ onClick (props.passFlashcard flashcard) ] [ text "Pass" ]
+                    div [ class "flex flex-col justify-between items-center grow" ]
+                        [ div [ class "flex flex-col gap-4" ]
+                            [ div [ class "text-2xl text-center" ] [ text flashcard.pinyin ]
+                            , div [ class "text-xl text-center" ] [ text (String.join "; " flashcard.definitions) ]
+                            ]
+                        , div [ class "w-full flex gap-3 text-lg" ]
+                            [ button
+                                [ onClick (props.failFlashcard flashcard)
+                                , class "grow basis-1 py-2 px-4 bg-red-600 rounded-lg uppercase"
+                                ]
+                                [ text "Fail" ]
+                            , button
+                                [ onClick (props.passFlashcard flashcard)
+                                , class "grow basis-1 py-2 px-4 bg-blue-600 rounded-lg uppercase"
+                                ]
+                                [ text "Pass" ]
                             ]
                         ]
 
@@ -107,7 +117,7 @@ view props =
                     div [ class "flex flex-col justify-end grow" ]
                         [ button
                             [ onClick props.showFlashcardBack
-                            , class "py-2 px-4 bg-blue-600 text-lg rounded-lg"
+                            , class "py-2 px-4 bg-blue-600 text-lg rounded-lg uppercase"
                             ]
                             [ text "Show back" ]
                         ]
